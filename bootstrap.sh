975d0672b84ccec973d9ca65d8c9ba9a23d68c0a
Sean Moubry <sean@moubry.com>
2015-02-26 09:09:52 -0600

    Add message about possible symlink failure
    I’m deliberately not forcing the creation of the symlink, because that
    would break any existing symlink you have, or worse. You’ll need to
    manually resolve this.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# Creation of symlink will fail if ~/bin already exists.
+# So you'll need to do a `rm -rf ~/bin`
+
 ln -s ~/Projects/dotfiles ~/bin
 echo 'source ~/Projects/dotfiles/dotfiles/bashrc' >> ~/.bash_profile
